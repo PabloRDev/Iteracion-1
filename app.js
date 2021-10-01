7b38f2ac230fa559b7862dd3bc93646b17dc954f
PabloRDev <pablord@MacBook-Air-de-Pablo.local>
2021-10-01 17:26:35 +0200

    Iteracion-3

--- app.js
+++ app.js
@@ -36,8 +36,14 @@ let car1 = { name: "BMW m&m", basePrice: 50000, finalPrice: 60000 };
 let car2 = { name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000 };
 
 globalBasePrice = 25000;
-car2.finalPrice = car2.basePrice+globalBasePrice;
-car1.finalPrice = car1.basePrice+globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
+car1.finalPrice = car1.basePrice + globalBasePrice;
 
 console.log(car2.finalPrice);
 // Iteración #3
+multiplication = 10 * 5;
+division = 10 / 2;
+y = 10; z = 5;
+x = y * z;
+
+// Iteración #4
